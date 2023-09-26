9e89dc228527c5bc6ba9c688a638d929d0572857
Ethan <ethankcratchley@gmail.com>
2023-09-26 12:01:39 -0700

    dicktionary.py

---
+++ dicktionary.py
@@ -0,0 +1,11 @@
+user_1 = {
+    'first_name' : 'tan',
+    'last_name' : 'smith',
+    'address' : 'python world 1543',
+    'phone_number' : '322 222 4324'
+    }
+
+print(user_1['first_name'])
+print(user_1['last_name'])
+print(user_1['address'])
+print(user_1['phone_number'])
