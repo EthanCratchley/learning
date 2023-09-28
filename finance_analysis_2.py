9599242b60fc243cb15892a151d99527ef2dbb0f
Ethan <ethankcratchley@gmail.com>
2023-09-28 16:50:35 -0700

    finance_analysis_2.py

--- finance_analysis_2.py
+++ finance_analysis_2.py
@@ -2,6 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Read file path into dataframe
 df = pd.read_csv('/Users/ethancratchley/desktop/appl_income_02.csv')
 
-print(df.head())
+# Removing rows based on index
+df.drop([5, 6, 15, 20, 24, 33, 34, 35, 37, 41, 42, 43, 44, 46], inplace=True)
+
+# Removing Column
+df.drop(df.columns[[6]], axis=1, inplace=True)
+
+# Print head of dataframe
+print(df.head(10))
+
+# Save the cleaned Dataframe
+df.to_csv('/Users/ethancratchley/desktop/appl_income_03.csv', index=False)
